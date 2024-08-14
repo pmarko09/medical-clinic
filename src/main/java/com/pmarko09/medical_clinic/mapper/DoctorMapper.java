dde4ec945df15d3bc421772acd9917cfbdc3b960
pmarko <p.marko09@yahoo.com>
2024-08-14 10:47:28 +0200

    DoctorMapper updated using MapStruct

--- src/main/java/com/pmarko09/medical_clinic/mapper/DoctorMapper.java
+++ src/main/java/com/pmarko09/medical_clinic/mapper/DoctorMapper.java
@@ -1,15 +1,53 @@
 package com.pmarko09.medical_clinic.mapper;
 
+import com.pmarko09.medical_clinic.exception.PatientNotFoundException;
 import com.pmarko09.medical_clinic.model.Doctor;
 import com.pmarko09.medical_clinic.model.DoctorDTO;
+import com.pmarko09.medical_clinic.model.Patient;
+import com.pmarko09.medical_clinic.repository.PatientRepository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "string")
-public interface DoctorMapper {
+public abstract class DoctorMapper {
 
-    DoctorDTO toDto(Doctor doctor);
+    @Autowired
+    protected PatientRepository patientRepository;
 
-    Doctor toDoctor(DoctorDTO doctorDTO);
+    @Mapping(source = "patientList", target = "patients", qualifiedByName = "patientsListToEmails")
+    public abstract DoctorDTO toDto(Doctor doctor);
+
+    @Mapping(source = "patients", target = "patientList", qualifiedByName = "emailsToPatientList")
+    public abstract Doctor toDoctor(DoctorDTO doctorDTO);
+
+    @Named("patientsListToEmails")
+    List<String> patientsListToEmails(List<Patient> patients) {
+        if (patients == null || patients.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return patients.stream()
+                .map(Patient::getEmail)
+                .toList();
+    }
+
+    @Named("emailsToPatientList")
+    List<Patient> emailsToPatientList(List<String> emails) {
+        if (emails == null || emails.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return emails.stream()
+                .map(email -> {
+                    return patientRepository.getPatient(email)
+                            .orElseThrow(() -> new PatientNotFoundException(email));
+                })
+                .collect(Collectors.toList());
+    }
 
 }
 
--- src/main/java/com/pmarko09/medical_clinic/model/Doctor.java
+++ src/main/java/com/pmarko09/medical_clinic/model/Doctor.java
@@ -1,10 +1,14 @@
 package com.pmarko09.medical_clinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Doctor {
 
     private String firstName;
