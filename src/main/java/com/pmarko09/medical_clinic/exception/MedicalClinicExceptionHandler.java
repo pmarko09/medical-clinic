f6ffb44df4cc37d9005ae2672b1e870027b2fe0c
pmarko <p.marko09@yahoo.com>
2024-08-21 18:32:01 +0200

    adding MedicalClinicExceptionHandler 2

--- src/main/java/com/pmarko09/medical_clinic/exception/MedicalClinicExceptionHandler.java
+++ src/main/java/com/pmarko09/medical_clinic/exception/MedicalClinicExceptionHandler.java
@@ -1,42 +1,51 @@
 package com.pmarko09.medical_clinic.exception;
 
+import com.pmarko09.medical_clinic.model.ErrorMessageDTO;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class MedicalClinicExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(PatientNotFoundException.class)
     protected ResponseEntity<Object> handlePatientNotFound(PatientNotFoundException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.NOT_FOUND);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
     }
 
     @ExceptionHandler(PatientAlreadyExistException.class)
     protected ResponseEntity<Object> handlePatientAlreadyExists(PatientAlreadyExistException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.CONFLICT);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
     }
 
     @ExceptionHandler(IllegalPatientDataException.class)
     protected ResponseEntity<Object> handleIllegalPatientData(IllegalPatientDataException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.BAD_REQUEST);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 
     @ExceptionHandler(DoctorNotFoundException.class)
     protected ResponseEntity<Object> handleDoctorNotFound(DoctorNotFoundException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.NOT_FOUND);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
     }
 
     @ExceptionHandler(DoctorAlreadyExistException.class)
     protected ResponseEntity<Object> handleDoctorAlreadyExists(DoctorAlreadyExistException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.CONFLICT);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
     }
 
     @ExceptionHandler(IllegalDoctorDataException.class)
     protected ResponseEntity<Object> handleIllegalDoctorData(IllegalDoctorDataException ex, WebRequest webRequest) {
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+        ErrorMessageDTO bodyResponse = new ErrorMessageDTO(ex.getMessage(), LocalDateTime.now(), HttpStatus.BAD_REQUEST);
+        return handleExceptionInternal(ex, bodyResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
     }
 
     @ExceptionHandler(Exception.class)
---
+++ src/main/java/com/pmarko09/medical_clinic/model/ErrorMessageDTO.java
@@ -0,0 +1,17 @@
+package com.pmarko09.medical_clinic.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+    private String message;
+    private LocalDateTime time;
+    private HttpStatus httpStatus;
+
+}
