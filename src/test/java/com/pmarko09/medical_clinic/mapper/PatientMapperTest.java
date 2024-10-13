c80062ef092af462c9bead15f7e5f93bca616e8d
pmarko <p.marko09@yahoo.com>
2024-10-13 18:10:54 +0200

    adding PatientMapperTest and fix liquibase

--- src/test/java/com/pmarko09/medical_clinic/mapper/PatientMapperTest.java
+++ src/test/java/com/pmarko09/medical_clinic/mapper/PatientMapperTest.java
@@ -1,5 +1,8 @@
 package com.pmarko09.medical_clinic.mapper;
 
+import com.pmarko09.medical_clinic.model.dto.PatientDTO;
+import com.pmarko09.medical_clinic.model.model.Patient;
+import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,4 +11,22 @@ public class PatientMapperTest {
 
     PatientMapper patientMapper = Mappers.getMapper(PatientMapper.class);
 
+    @Test
+    void mapPatientToDTO() {
+
+        Patient patient = new Patient();
+        patient.setId(1L);
+        patient.setFirstName("Jan");
+        patient.setLastName("X");
+        patient.setPhoneNumber("1234");
+        patient.setIdCardNo("0000");
+
+        PatientDTO result = patientMapper.toDto(patient);
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+        assertEquals("Jan", result.getFirstName());
+        assertEquals("X", result.getLastName());
+        assertEquals("1234", result.getPhoneNumber());
+    }
 }
